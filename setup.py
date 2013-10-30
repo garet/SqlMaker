c8e4750f54b5feabe180811e0ccd23eeb4eb37e0
garet <garet@d3j.org>
2013-10-30 23:21:05 +0400

    Change version packet.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from os.path import join, dirname
 
 setup(
     name='SqlMaker',
-    version='0.1.1a',
+    version='0.1.2a',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
 )
